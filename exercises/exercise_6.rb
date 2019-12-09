9e06e251bada2bce760edfb197c822dced4f7033
Kevin Laburte <kevin.laburte@gmail.com>
2019-12-09 16:34:20 -0500

    Implement exercise #6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,26 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khuam", last_name: "Vi", hourly_rate: 50)
+@store1.employees.create(first_name: "Paul", last_name: "Baker", hourly_rate: 40)
+@store2.employees.create(first_name: "Leila", last_name: "Smith", hourly_rate: 45)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 65)
+
+# Exercise 6: One-to-many association
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a store_id (integer) column. This is a column that identifies which store each employee belongs to. It points to the id (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the store_id column.
+
+# Add the following code directly inside the Store model (class): has_many :employees
+# Add the following code directly inside the Employee model (class): belongs_to :store
+# Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+# Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the @store# instance variables that you defined in previous exercises. Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+
+# create_table :employees do |table|
+#   table.references :store
+#   table.column :first_name, :string
+#   table.column :last_name, :string
+#   table.column :hourly_rate, :integer
+#   table.timestamps null: false
+# end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates_associated :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_line
 
+  def must_carry_line
+    if (!mens_apparel && !womens_apparel) then
+      errors.add(:mens_apparel, "Store must carry a product line")
+      errors.add(:womens_apparel, "Store must carry a product line")
+    end
+
+  end
 end
