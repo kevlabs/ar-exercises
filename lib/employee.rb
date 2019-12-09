abe57c513a3525facd6b744e4d12fd27e108b960
Kevin Laburte <kevin.laburte@gmail.com>
2019-12-09 17:42:19 -0500

    implement password generator

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,21 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
   validates :store_id, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+
+  before_save :generate_password, on: :create
+
+  private
+
+    def generate_password
+      self.password = ""
+      chars = [*(0..9), *('a'..'z'), *('A'..'Z')]
+      8.times do
+        self.password += chars[rand(chars.length - 1)].to_s
+      end
+    end
   
 end
